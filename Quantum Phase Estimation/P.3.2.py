18a8d6014198fd6050a1ced7ab389942f824554b
Adrià Sansa Perna <adria@asansa.eu>
2024-08-21 14:41:05 +0200

    Finally corrected the QPE 3.2

--- Quantum Phase Estimation/P.3.2.py
+++ Quantum Phase Estimation/P.3.2.py
@@ -6,6 +6,8 @@ Created on Tue Aug 20 17:07:40 2024
 @author: adria
 """
 
+# This code will only run directly on Pennylane's codebook!!!
+
 dev = qml.device("default.qubit", wires=10)
 
 def estimates_array(unitary):
@@ -18,14 +20,13 @@ def estimates_array(unitary):
         [(float, float)]: a list of phase windows for 2 to 9 
         estimation wires
     """
-    num_estimation_wires = 2
+
     estimates = []
-    estimation_wires_loop = [range(0,i) for i in range(1, 10)]
+    estimation_wires_loop = [range(0,i) for i in range(2, 10)]
     target_wires = [9]
-    
-    for i in range( 9 ):
+    #print(estimation_wires_loop)
+    for i in range( 8 ):
         estimation_wires = estimation_wires_loop[i]
-        print(estimation_wires)
         probs = qpe(unitary, estimation_wires, target_wires)
         estimates.append( phase_window(probs, estimation_wires) )
 
@@ -36,7 +37,7 @@ U = np.array([[1, 0], [0, np.exp((2*np.pi*1j/7))]])
 
 estimates_array(U)
 
-#print(estimates_array(U))
+print(estimates_array(U))
 
 ###################
 # SUBMIT FOR PLOT #
