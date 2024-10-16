8fd1d7490fef0b0f628b7a0ac47e6ce282c7cf25
Adrià Sansa Perna <adria@asansa.eu>
2024-10-16 18:53:44 +0200

    Getting closer...

--- Challenges/QML/3_qubit.py
+++ Challenges/QML/3_qubit.py
@@ -8,25 +8,39 @@ Created on Tue Oct 15 14:05:49 2024
 
 import pennylane as qml
 import pennylane.numpy as np
+import matplotlib.pyplot as plt
 
 dev = qml.device("default.qubit", wires=3)
 
-alpha = np.linspace(0, 10, 100)
-theta = np.linspace(0, 10, 100)
-results = np.zeros([100, 100])
-limit = 0.5
+dim_Alpha = 200
+dim_Theta = 200
+alpha = np.linspace(0, 10, dim_Alpha)
+theta = np.linspace(0, 10, dim_Theta)
+results = np.zeros([dim_Alpha, dim_Theta, 8])
 
-for i in alpha:
-    for j in theta:
+limit = 0.9
+y = np.zeros([dim_Theta, 8])
+
+for i in range(len(alpha)):
+    for j in range(len(theta)):
         @qml.qnode(dev)
         def circuit(alpha, theta):
             qml.RY(theta, wires = 0)
             qml.RY(theta, wires = 1)
             qml.Toffoli((0,1,2))
             qml.RY(70/9*theta, wires = 0)
-            qml.RY(theta, wires = 1)
+            qml.RY(alpha*theta, wires = 1)
             return qml.probs(wires = [0,1,2])
         
-        results[i, j] = circuit(i, j)
+        results[i, j, :] = circuit(alpha[i], theta[j])
+
+    if (max(results[i, :, 0]) > limit) & (max(results[i, :, 1]) > limit) & (max(results[i, :, 2]) > limit) & (max(results[i, :, 3]) > limit) & (max(results[i, :, 4]) > limit) & (max(results[i, :, 5]) > limit) & (max(results[i, :, 6]) > limit) & (max(results[i, :, 7]) > limit):
+       print(alpha[i])
+       
+       for k in range(8):
+           y[:,k] = results[i, :, k]
+           plt.plot(y[:,k])
+       break
+    
 
 # circuit = [for i in theta]
